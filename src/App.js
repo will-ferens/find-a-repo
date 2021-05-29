3d1b34e7612a25925a3d89b7e007737922427944
_dream <will.ferens@gmail.com>
2021-05-29 11:41:26 -0600

    initialized parental component Search, created Header, styled divs for App, handle search functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import SearchInput from './components/SearchInput'
+import Header from './components/Header'
+import Search from './components/Search'
 import styled from 'styled-components'
 
 export const Global = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
+
 function App() {
   return (
     <Global>
-      <SearchInput />
+      <Header />
+      <Search />
     </Global>
   );
 }
