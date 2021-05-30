a53ca75a63f5850ff58520562b58410089e00d9b
_dream <will.ferens@gmail.com>
2021-05-30 14:45:08 -0600

    styled results list, refactored list component to be DRY

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,7 +4,46 @@ import { Link } from 'react-router-dom'
 
 export const List = styled.ul`
     list-style: none;
+    margin: 0;
+    padding: 0;
 `
+export const ListItemWrapper = styled.li`
+    margin: 0;
+    padding: 12px;
+    border-radius: 10px;
+    box-shadow: 0 5px 7px -1px rgba(51, 51, 51, 0.23);
+    a {
+        color: #333;
+        text-decoration: none;
+    }
+`
+
+export const ItemHeader = styled.div`
+    padding-bottom: 6px;
+`
+
+export const ItemDetails = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const ListItem = ({ result }) => {
+    return (
+        <ListItemWrapper key={result.id}>
+            <Link to={`/repo/${result.id}`}>
+                <ItemHeader>
+                    {result.owner.login} / {result.name}
+                </ItemHeader>
+                <ItemDetails>
+                    <span>⭐️ { result.stargazers_count }</span>
+                    <span>{ result.language }</span>
+                    <span>{ result.private ? '🔒' : '📖'}</span>
+                </ItemDetails>
+            </Link>
+        </ListItemWrapper>
+    )
+}
 
 const ResultsList = ({ results, filterActive, filteredResults }) => {
     if(filterActive) {
@@ -13,32 +52,24 @@ const ResultsList = ({ results, filterActive, filteredResults }) => {
                 {
                     filteredResults ? filteredResults.map((result) => {
                         return (
-                            <li key={result.id}>
-                                <Link to={`/repo/${result.id}`}>
-                                    {result.name}
-                                </Link>
-                            </li>
+                            <ListItem result={result} />
                         ) 
                     }) : null
                 }
             </List>
         )
     }
-        return (
-            <List>
-                {
-                    results ? results.map((result) => {
-                        return (
-                            <li key={result.id}>
-                                <Link to={`/repo/${result.id}`}>
-                                    {result.name}
-                                </Link>
-                            </li>
-                        ) 
-                    }) : null
-                }
-            </List>
-        )
+    return (
+        <List>
+            {
+                results ? results.map((result) => {
+                    return (
+                        <ListItem result={result} />
+                    ) 
+                }) : null
+            }
+        </List>
+    )
 }
 
 export default ResultsList
